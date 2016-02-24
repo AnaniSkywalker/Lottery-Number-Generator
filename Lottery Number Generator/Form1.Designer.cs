f427798fb70be76e11d7a2044bfa81969de0ae1a
AnaniSkywalker <Artificial_Intelligence@Michael-of-God.local>
2016-02-24 08:43:04 -0600

    Commits

---
+++ Lottery Number Generator/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿// Author: Anani Assoutovi
+namespace Lottery_Number_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstLabel1 = new System.Windows.Forms.Label();
+            this.secondLabel2 = new System.Windows.Forms.Label();
+            this.thirdLabel3 = new System.Windows.Forms.Label();
+            this.fourthLabel4 = new System.Windows.Forms.Label();
+            this.fifthLabel5 = new System.Windows.Forms.Label();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.MegaBallLabel = new System.Windows.Forms.Label();
+            this.MegaButton = new System.Windows.Forms.Button();
+            this.MegaBallLabel1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstLabel1
+            // 
+            this.firstLabel1.BackColor = System.Drawing.Color.Gold;
+            this.firstLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.firstLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.firstLabel1.Location = new System.Drawing.Point(12, 57);
+            this.firstLabel1.Name = "firstLabel1";
+            this.firstLabel1.Size = new System.Drawing.Size(50, 26);
+            this.firstLabel1.TabIndex = 0;
+            // 
+            // secondLabel2
+            // 
+            this.secondLabel2.BackColor = System.Drawing.Color.Gold;
+            this.secondLabel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.secondLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondLabel2.Location = new System.Drawing.Point(87, 57);
+            this.secondLabel2.Name = "secondLabel2";
+            this.secondLabel2.Size = new System.Drawing.Size(50, 26);
+            this.secondLabel2.TabIndex = 1;
+            // 
+            // thirdLabel3
+            // 
+            this.thirdLabel3.BackColor = System.Drawing.Color.Gold;
+            this.thirdLabel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.thirdLabel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.thirdLabel3.Location = new System.Drawing.Point(163, 57);
+            this.thirdLabel3.Name = "thirdLabel3";
+            this.thirdLabel3.Size = new System.Drawing.Size(50, 26);
+            this.thirdLabel3.TabIndex = 2;
+            // 
+            // fourthLabel4
+            // 
+            this.fourthLabel4.BackColor = System.Drawing.Color.Gold;
+            this.fourthLabel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fourthLabel4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fourthLabel4.Location = new System.Drawing.Point(235, 57);
+            this.fourthLabel4.Name = "fourthLabel4";
+            this.fourthLabel4.Size = new System.Drawing.Size(50, 26);
+            this.fourthLabel4.TabIndex = 3;
+            // 
+            // fifthLabel5
+            // 
+            this.fifthLabel5.BackColor = System.Drawing.Color.Gold;
+            this.fifthLabel5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.fifthLabel5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fifthLabel5.Location = new System.Drawing.Point(317, 57);
+            this.fifthLabel5.Name = "fifthLabel5";
+            this.fifthLabel5.Size = new System.Drawing.Size(50, 26);
+            this.fifthLabel5.TabIndex = 4;
+            // 
+            // generateButton
+            // 
+            this.generateButton.BackColor = System.Drawing.Color.Gold;
+            this.generateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.generateButton.Location = new System.Drawing.Point(16, 148);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(137, 35);
+            this.generateButton.TabIndex = 5;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = false;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click_1);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.Red;
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(185, 259);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(134, 35);
+            this.exitButton.TabIndex = 6;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click_1);
+            // 
+            // MegaBallLabel
+            // 
+            this.MegaBallLabel.AutoSize = true;
+            this.MegaBallLabel.BackColor = System.Drawing.Color.Coral;
+            this.MegaBallLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MegaBallLabel.Location = new System.Drawing.Point(438, 65);
+            this.MegaBallLabel.Name = "MegaBallLabel";
+            this.MegaBallLabel.Size = new System.Drawing.Size(0, 24);
+            this.MegaBallLabel.TabIndex = 7;
+            // 
+            // MegaButton
+            // 
+            this.MegaButton.BackColor = System.Drawing.Color.GreenYellow;
+            this.MegaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MegaButton.Location = new System.Drawing.Point(399, 148);
+            this.MegaButton.Name = "MegaButton";
+            this.MegaButton.Size = new System.Drawing.Size(134, 35);
+            this.MegaButton.TabIndex = 8;
+            this.MegaButton.Text = "Generate";
+            this.MegaButton.UseVisualStyleBackColor = false;
+            this.MegaButton.Click += new System.EventHandler(this.MegaButton_Click);
+            // 
+            // MegaBallLabel1
+            // 
+            this.MegaBallLabel1.BackColor = System.Drawing.Color.GreenYellow;
+            this.MegaBallLabel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.MegaBallLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MegaBallLabel1.Location = new System.Drawing.Point(444, 57);
+            this.MegaBallLabel1.Name = "MegaBallLabel1";
+            this.MegaBallLabel1.Size = new System.Drawing.Size(50, 26);
+            this.MegaBallLabel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(546, 417);
+            this.Controls.Add(this.MegaBallLabel1);
+            this.Controls.Add(this.MegaButton);
+            this.Controls.Add(this.MegaBallLabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.fifthLabel5);
+            this.Controls.Add(this.fourthLabel4);
+            this.Controls.Add(this.thirdLabel3);
+            this.Controls.Add(this.secondLabel2);
+            this.Controls.Add(this.firstLabel1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+//        private System.Windows.Forms.Label firstLabel;
+//        private System.Windows.Forms.Label secondLabel;
+//        private System.Windows.Forms.Label thirdLabel;
+//        private System.Windows.Forms.Label fourthLabel;
+//        private System.Windows.Forms.Label fifthLabel;
+//        private System.Windows.Forms.Button button1;
+//        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label firstLabel1;
+        private System.Windows.Forms.Label secondLabel2;
+        private System.Windows.Forms.Label thirdLabel3;
+        private System.Windows.Forms.Label fourthLabel4;
+        private System.Windows.Forms.Label fifthLabel5;
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label MegaBallLabel;
+        private System.Windows.Forms.Button MegaButton;
+        private System.Windows.Forms.Label MegaBallLabel1;
+    }
+}
+
---
+++ Lottery Number Generator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Number_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click_1(object sender, EventArgs e)
+        {
+            // Create an array to hold the numbers.
+            const int SIZE = 5;
+            int[] lotteryNumbers = new int[SIZE];
+
+            // Create a Random object.
+            Random rand = new Random();
+
+            // Fill the array with random numbers, in the range of 0 to 75.
+            for (int index = 0; index < lotteryNumbers.Length; index++)
+            {
+                lotteryNumbers[index] = rand.Next(75);
+            }
+
+            // Display the array elements in the Label controls.
+            firstLabel1.Text = lotteryNumbers[0].ToString();
+            secondLabel2.Text = lotteryNumbers[1].ToString();
+            thirdLabel3.Text = lotteryNumbers[2].ToString();
+            fourthLabel4.Text = lotteryNumbers[3].ToString();
+            fifthLabel5.Text = lotteryNumbers[4].ToString();
+        }
+
+        private void exitButton_Click_1(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void MegaButton_Click(object sender, EventArgs e)
+        {
+
+            // Create an array to hold the numbers.
+            const int SIZE = 1;
+            int[] lotteryNumbers = new int[SIZE];
+
+            // Create a Random object.
+            Random rand = new Random();
+
+            // Fill the array with random numbers, in the range of 0 to 15.
+            for (int index = 0; index < lotteryNumbers.Length; index++)
+            {
+                lotteryNumbers[index] = rand.Next(15);
+            }
+
+            // Display the array elements in the Label controls.
+            MegaBallLabel1.Text = lotteryNumbers[0].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
